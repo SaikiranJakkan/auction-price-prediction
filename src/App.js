b873b50fb422f7082d3e6ec72d933bea676f1069
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-10-25 03:29:59 +0530

    Modify form

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import React, { useState } from "react";
 function App() {
   const [type, setType] = useState("");
   const [runs, setRuns] = useState(0);
-  const [balls, setBalls] = useState(0);
-  const [avg, setAvg] = useState(0);
+  const [ballsFaced, setBallsFaced] = useState(0);
+  const [battingAvg, setBattingAvg] = useState(0);
   const [strikeRate, setStrikeRate] = useState(0);
+
+  const [ballsBowled, setBallsBowled] = useState(0);
+  const [bowlingAvg, setBowlingAvg] = useState(0);
+  const [economy, setEconomy] = useState(0);
+  const [wickets, setWickets] = useState(0);
 
   function handleSubmit() {
     console.log("Submitted");
@@ -19,52 +24,102 @@ function App() {
           <input type="radio" value={type} name="type" onChange={(e) => setType("batsman")} /> Batsman
           <input type="radio" value={type} name="type" onChange={(e) => setType("bowler")}/> Bowler
 
-          <div className="form-group">
-            <label>
-              Runs
-              <input
-                type="number"
-                value={runs}
-                onChange={(e) => setRuns(e.target.value)}
-              />
-            </label>
-          </div>
+          { type === "batsman" ? <div>
+            <div className="form-group">
+              <label>
+                Runs scored
+                <input
+                  type="number"
+                  value={runs}
+                  onChange={(e) => setRuns(e.target.value)}
+                />
+              </label>
+            </div>
 
-          <div className="form-group">
-            <label>
-              Balls
-              <input
-                type="number"
-                value={balls}
-                onChange={(e) => setBalls(e.target.value)}
-              />
-            </label>
-          </div>
+            <div className="form-group">
+              <label>
+                Balls Faced
+                <input
+                  type="number"
+                  value={ballsFaced}
+                  onChange={(e) => setBallsFaced(e.target.value)}
+                />
+              </label>
+            </div>
 
-          <div className="form-group">
-            <label>
-              Average
-              <input
-                type="number"
-                step="0.1"
-                value={avg}
-                onChange={(e) => setAvg(e.target.value)}
-              />
-            </label>
-          </div>
+            <div className="form-group">
+              <label>
+                Batting Average
+                <input
+                  type="number"
+                  step="0.1"
+                  value={battingAvg}
+                  onChange={(e) => setBattingAvg(e.target.value)}
+                />
+              </label>
+            </div>
 
-          <div className="form-group">
-            <label>
-              Strike Rate
-              <input
-                type="number"
-                step="0.1"
-                value={strikeRate}
-                onChange={(e) => setStrikeRate(e.target.value)}
-              />
-            </label>
+            <div className="form-group">
+              <label>
+                Strike Rate
+                <input
+                  type="number"
+                  step="0.1"
+                  value={strikeRate}
+                  onChange={(e) => setStrikeRate(e.target.value)}
+                />
+              </label>
+            </div>
           </div>
+          :
+          <div> 
+            <div className="form-group">
+              <label>
+                Balls bowled
+                <input
+                  type="number"
+                  value={ballsBowled}
+                  onChange={(e) => setBallsBowled(e.target.value)}
+                />
+              </label>
+            </div>
 
+            <div className="form-group">
+              <label>
+                Bowling Average
+                <input
+                  type="number"
+                  step="0.1"
+                  value={bowlingAvg}
+                  onChange={(e) => setBowlingAvg(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div className="form-group">
+              <label>
+                Economy
+                <input
+                  type="number"
+                  step="0.1"
+                  value={economy}
+                  onChange={(e) => setEconomy(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div className="form-group">
+              <label>
+                Wickets
+                <input
+                  type="number"
+                  value={wickets}
+                  onChange={(e) => setWickets(e.target.value)}
+                />
+              </label>
+            </div>
+          </div> 
+          }
           <input type="submit" />
         </form>
       </div>
