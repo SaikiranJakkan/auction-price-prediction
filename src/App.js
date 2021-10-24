5a90aae71ddb2fa7a7515b9c104bf79f65932e31
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-10-25 01:24:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [type, setType] = useState("");
+  const [runs, setRuns] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [avg, setAvg] = useState(0);
+  const [strikeRate, setStrikeRate] = useState(0);
+
+  function handleSubmit() {
+    console.log("Submitted");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> 
+        <form onSubmit={handleSubmit}>
+          <input type="radio" value={type} name="type" onChange={(e) => setType("batsman")} /> Batsman
+          <input type="radio" value={type} name="type" onChange={(e) => setType("bowler")}/> Bowler
+
+          <div className="form-group">
+            <label>
+              Runs
+              <input
+                type="number"
+                value={runs}
+                onChange={(e) => setRuns(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label>
+              Balls
+              <input
+                type="number"
+                value={balls}
+                onChange={(e) => setBalls(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label>
+              Average
+              <input
+                type="number"
+                step="0.1"
+                value={avg}
+                onChange={(e) => setAvg(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label>
+              Strike Rate
+              <input
+                type="number"
+                step="0.1"
+                value={strikeRate}
+                onChange={(e) => setStrikeRate(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 }
